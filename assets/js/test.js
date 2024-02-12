5108378558ce83ba2e0e6b9cb1d0ed3400a9e749
setareh <130979348+setareh0@users.noreply.github.com>
2024-02-13 02:09:06 +0330

    ...

--- assets/js/test.js
+++ assets/js/test.js
@@ -32,3 +32,33 @@ $(document).ready(function() {
     count(0, counter[j], j);
   }
 });
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
